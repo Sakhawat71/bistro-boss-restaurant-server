aa163449246fc2e4d408e113c9fc4d981d157af3
sakhawat71 <sakhawat.showrav@gmail.com>
2024-03-09 23:34:32 +0600

    json web token

--- index.js
+++ index.js
@@ -30,26 +30,34 @@ async function run() {
         const bistroCart = client.db('bistroBoss').collection('carts');
         const bistroUser = client.db('bistroBoss').collection('user');
 
+        // jwt
+        app.post("/api/v1/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign("token",process.env.JWT_SECRET,{
+                expiresIn: '1h',
+            })
+        })
 
-        app.get("/api/v1/all-user", async(req,res)=> {
+
+        app.get("/api/v1/all-user", async (req, res) => {
             const result = await bistroUser.find().toArray();
             res.send(result);
         })
 
-        app.post("/api/v1/add-user", async(req,res)=>{
+        app.post("/api/v1/add-user", async (req, res) => {
             const user = req.body;
 
-            const query = {email : user.email}
+            const query = { email: user.email }
             const existingUser = await bistroUser.findOne(query)
-            if(existingUser){
-                return res.send({message: "user already exist",insertedId: null})
+            if (existingUser) {
+                return res.send({ message: "user already exist", insertedId: null })
             }
 
             const result = await bistroUser.insertOne(user);
             res.send(result);
         })
 
-        app.delete('/api/v1/delete-user/:id', async(req,res)=>{
+        app.delete('/api/v1/delete-user/:id', async (req, res) => {
 
             try {
                 const id = req.params.id;
@@ -62,21 +70,21 @@ async function run() {
             }
         })
 
-        app.patch("/api/v1/make-admin/:id", async(req,res)=>{
-            try{
+        app.patch("/api/v1/make-admin/:id", async (req, res) => {
+            try {
                 const id = req.params.id;
-                const filter = {_id : new ObjectId(id)}
+                const filter = { _id: new ObjectId(id) }
                 const updateDoc = {
                     $set: {
-                        role : "admin"
+                        role: "admin"
                     }
                 }
-                const result = await bistroUser.updateOne(filter,updateDoc);
+                const result = await bistroUser.updateOne(filter, updateDoc);
                 res.send(result)
             }
-            catch(error){
+            catch (error) {
                 console.log(error);
-                res.status(500).send({message:"can`t make admin"})
+                res.status(500).send({ message: "can`t make admin" })
             }
         })
 
@@ -91,26 +99,26 @@ async function run() {
             const result = await bistroReviews.find().toArray();
             res.send(result)
         })
-    
+
         // cart 
-        app.post("/api/v1/post-carts" , async(req,res) => {
+        app.post("/api/v1/post-carts", async (req, res) => {
             const cartItem = req.body;
             const result = await bistroCart.insertOne(cartItem);
             res.send(result);
         })
 
-        app.get("/api/v1/get-carts", async(req,res)=>{
+        app.get("/api/v1/get-carts", async (req, res) => {
 
             const email = req.query.email;
             // console.log({email})
-            const qeury = {email:email}
+            const qeury = { email: email }
             const result = await bistroCart.find(qeury).toArray();
             res.send(result)
         })
 
-        app.delete("/api/v1/delete-cart-item/:id" , async(req,res)=> {
+        app.delete("/api/v1/delete-cart-item/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await bistroCart.deleteOne(query);
             res.send(result)
         })
